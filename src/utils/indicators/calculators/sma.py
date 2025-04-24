b42076704989054dd660094a561c8d3ca1d56a12
McSnurtle <joepro17@outlook.com>
2025-04-24 12:19:14 -0400

    add sample 'simple moving average' indicator script

---
+++ src/utils/indicators/calculators/sma.py
@@ -0,0 +1,16 @@
+# imports
+import pandas as pd
+from utils.indicators import registry
+
+
+@registry.register_indicator("SMA")
+def calculate(df: pd.DataFrame, period: int = 50) -> pd.DataFrame:
+    """Returns a dataframe with the same dimensions as the input of the simple moving average
+
+    :param df: pd.DataFrame, the input chart data (return only calculates the ['close'].)
+    :param period: int, the period to "roll over" when calculating the average."""
+
+    return pd.DataFrame({
+        'time': df['date'],
+        f'SMA {period}': df['close'].rolling(window=period).mean()
+    }).dropna()
