1b5650cbff2dd02731a186578faafd8f9b25a524
McSnurtle <joepro17@outlook.com>
2025-04-25 19:11:56 -0400

    refactor available indicators

---
+++ src/utils/indicators/calculators/sma50.py
@@ -0,0 +1,17 @@
+#src/utils/indicators/calculators/sma50.py
+# imports
+import pandas as pd
+from utils.indicators import registry
+
+
+@registry.register_indicator("SMA")
+def calculate(df: pd.DataFrame) -> pd.DataFrame:
+    """Returns a dataframe with the same dimensions as the input of the simple moving average
+
+    :param df: pd.DataFrame, the input chart data (return only calculates the ['close'].)
+    """
+
+    return pd.DataFrame({
+        'time': df['date'],
+        'SMA': df['close'].rolling(window=50).mean()
+    }).dropna()
