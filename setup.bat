35a2d55acf56fc4ddff38ebafda8884896043262
McSnurtle <joepro17@outlook.com>
2025-04-25 17:21:03 -0400

    update setup scripts

--- setup.bat
+++ setup.bat
@@ -1,9 +1,6 @@
 @echo on
-if not exist venv\ (
-    echo Creating virtual environment
-    python3 --verbose -m venv venv
-)
-
+echo Attempting creation of virtual environment
+python3 --verbose -m venv venv
 .\venv\Scripts\activate
 pip3 install --upgrade --verbose -r requirements.txt
 python3 src/main.py
--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-if [ ! -d "./venv" ]; then
-    echo "Creating virtual environment"
-    python3 --verbose -m venv venv
-fi
-
+echo "Attempting creation of virtual environment"
+python3 --verbose -m venv venv
 source venv/bin/activate
 pip3 install --upgrade --verbose -r requirements.txt
 export PYWEBVIEW_GUI=qt
