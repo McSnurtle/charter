58a9a44420c65a96775c335789f2f786bd95f645
McSnurtle <93739492+McSnurtle@users.noreply.github.com>
2025-04-25 20:13:45 -0400

    fix silly setup.bat error
    explanation: https://stackoverflow.com/q/71915985

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @echo on
 echo Attempting creation of virtual environment
 python3 -m venv venv
-.\venv\Scripts\activate
+call .\venv\Scripts\activate
 pip3 install --upgrade --verbose -r requirements.txt
 python3 src/main.py
